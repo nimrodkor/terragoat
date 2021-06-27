9cd8f6edc528f135d650b230d6ad8b84dc80d0e7
Nimrod Kor <nimrod@bridgecrew.io>
2021-06-27 18:15:15 +0300

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -17,18 +17,6 @@ export AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMAAAKEY
 export AWS_DEFAULT_REGION=us-west-2
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 EOF
-  tags = merge({
-    Name = "${local.resource_prefix.value}-ec2"
-    }, {
-    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
-    git_file             = "terraform/aws/ec2.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "347af3cd-4f70-4632-aca3-4d5e30ffc0b6"
-  })
 }
 
 resource "aws_ebs_volume" "web_host_storage" {
